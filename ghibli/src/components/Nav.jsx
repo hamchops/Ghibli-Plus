66784fc598da9bec785c069dd2ed23bf9579f207
hamchopsgit config --global user.name hamchopsgit config --global user.email martinezlerenzo@gmail.com <lerenzomartinez@Lerenzos-Air.attlocal.net>
2022-11-09 18:04:35 -0600

    testing readme changes

--- ghibli/src/components/Nav.jsx
+++ ghibli/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Nav (){
     return (
         <div className="navbar">
-           <btn> <Link className="homebtn" to = "/"> Home </Link></btn>
+            <Link className="homebtn" to = "/"> Home </Link>
             <Link className="filmbtn" to = "/films"> Films </Link>
             <Link className="characterbtn" to = "/species"> Characters </Link>
 
